1268cfea8095aa1cd33fadd514e77e857d3a5e03
Alperen Sayar <alperensayar@MacBook-Pro.local>
2023-06-11 02:45:26 +0300

    venv2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
@@ -10,3 +11,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='0.0.0.0', port=8000, workers=4, reload=True) 
