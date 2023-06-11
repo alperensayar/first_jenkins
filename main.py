49ad6190115e22257894b244be90f6126baafaaf
Alperen Sayar <alperensayar@MacBook-Pro.local>
2023-06-11 03:08:33 +0300

    lastverv

--- main.py
+++ main.py
@@ -12,6 +12,3 @@ def read_root():
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
-
-if __name__ == '__main__':
-    uvicorn.run('main:app', port=8000, workers=4, reload=True) 
