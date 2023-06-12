967655451d90938a16c4a7b283f1afa8d3696fc6
Alperen Sayar <alperensayar@MacBook-Pro.local>
2023-06-12 17:11:52 +0300

    last one

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"deneme": "venv"}
+    return {"deneme": "deneme"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
