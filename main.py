7005aafd38726d5a727a50508f12bd4466ada890
Alperen Sayar <alperensayar@MacBook-Pro.local>
2023-06-12 15:57:14 +0300

    push last for venv

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Successs": "sdfsdalşkfsadşlkfşasdlfasd"}
+    return {"deneme": "venv"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
