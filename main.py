4f4995bc2d5f6161d65e7d6f49a94ff30a83e187
Alperen Sayar <alperensayar@MacBook-Pro.local>
2023-06-20 13:56:15 +0300

    latest review

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"deneme": "deneme2"}
+    return {"deneme": "deneme23"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
